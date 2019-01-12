606919556ac764090fa14c9e2b79895e0306f947
joyqi <magike.net@gmail.com>
2019-01-12 13:30:25 +0800

    add `is` method add more devices

--- README.md
+++ README.md
@@ -13,6 +13,15 @@ Returns GPU Model Name (eg: Apple A8 GPU).
 
 Returns device models list (eg: ["iPad Air 2", "iPad Mini 4"]).
 
+## MobileDevice.is(match)
+
+Detect device model.
+
+```
+MobileDevice.is('iPhone')
+MobileDevice.is('iPhone 6s')
+```
+
 # TODO
 
 1. Android Devices :)
--- device.js
+++ device.js
@@ -1,31 +1,30 @@
 (function () {
     var canvas, gl, glRenderer, models,
-        devices = {
-            "Apple A7 GPU": {
-                1136: ["iPhone 5", "iPhone 5s"],
-                2048: ["iPad Air", "iPad Mini 2", "iPad Mini 3"]
-            },
-
-            "Apple A8 GPU": {
-                1136: ["iPod touch (6th generation)"],
-                1334: ["iPhone 6"],
-                2001: ["iPhone 6 Plus"],
-                2048: ["iPad Air 2", "iPad Mini 4"]
-            },
-
-            "Apple A9 GPU": {
-                1136: ["iPhone SE"],
-                1334: ["iPhone 6s"],
-                2001: ["iPhone 6s Plus"],
-                2224: ["iPad Pro (9.7-inch)"],
-                2732: ["iPad Pro (12.9-inch)"]
-            },
-
-            "Apple A10 GPU": {
-                1334: ["iPhone 7"],
-                2001: ["iPhone 7 Plus"]
-            }
-        };
+        devices = [
+            ['a7', '640x1136', ['iPhone 5', 'iPhone 5s']],
+            ['a7', '1536x2048', ['iPad Air', 'iPad Mini 2', 'iPad Mini 3']],
+            ['a8', '640x1136', ['iPod touch (6th gen)']],
+            ['a8', '750x1334', ['iPhone 6']],
+            ['a8', '1242x2208', ['iPhone 6 Plus']],
+            ['a8', '1536x2048', ['iPad Air 2', 'iPad Mini 4']],
+            ['a9', '640x1136', ['iPhone SE']],
+            ['a9', '750x1334', ['iPhone 6s']],
+            ['a9', '1242x2208', ['iPhone 6s Plus']],
+            ['a9x', '1536x2048', ['iPad Pro (1st gen 9.7-inch)']],
+            ['a9x', '2048x2732', ['iPad Pro (1st gen 12.9-inch)']],
+            ['a10', '750x1334', ['iPhone 7']],
+            ['a10', '1242x2208', ['iPhone 7 Plus']],
+            ['a10x', '1668x2224', ['iPad Pro (2th gen 10.5-inch)']],
+            ['a10x', '2048x2732', ['iPad Pro (2th gen 12.9-inch)']],
+            ['a11', '750x1334', ['iPhone 8']],
+            ['a11', '1242x2208', ['iPhone 8 Plus']],
+            ['a11', '1125x2436', ['iPhone X']],
+            ['a12', '826x1792', ['iPhone Xr']],
+            ['a12', '1125x2436', ['iPhone Xs']],
+            ['a12', '1242x2688', ['iPhone Xs Max']],
+            ['a12x', '1668x2388', ['iPad Pro (3rd gen 11-inch)']],
+            ['a12x', '2048x2732', ['iPad Pro (3rd gen 12.9-inch)']]
+        ];
 
     function getCanvas() {
         if (canvas == null) {
@@ -43,8 +42,10 @@
         return gl;
     }
 
-    function getScreenWidth() {
-        return Math.max(screen.width, screen.height) * (window.devicePixelRatio || 1);
+    function getResolution() {
+        var ratio = window.devicePixelRatio || 1;
+        return (Math.min(screen.width, screen.height) * ratio)
+            + 'x' + (Math.max(screen.width, screen.height) * ratio);
     }
 
     function getGlRenderer() {
@@ -58,15 +59,21 @@
 
     function getModels() {
         if (models == null) {
-            var device = devices[getGlRenderer()];
+            var gpu = getGlRenderer(),
+                matches = gpu.match('/apple\s+([_a-z0-9-]+)\s+gpu/i'),
+                res = getResolution();
+            
+            models = ['unknown'];
 
-            if (device == undefined) {
-                models = ['unknown'];
-            } else {
-                models = device[getScreenWidth()];
+            if (matches) {
+                for (var i = 0; i < devices.length; i ++) {
+                    var device = devices[i];
 
-                if (models == undefined) {
-                    models = ['unknown'];
+                    if (matches[1].toLowerCase() == device[0]
+                        && res == device[1]) {
+                        models = device[2];
+                        break;
+                    }
                 }
             }
         }
@@ -80,4 +87,19 @@
 
     window.MobileDevice.getGlRenderer = getGlRenderer;
     window.MobileDevice.getModels = getModels;
+
+    window.MobileDevice.is = function (match) {
+        var currentModels = getModels();
+        match = match.toLowerCase().replace(/\s+$/, '') + ' ';
+
+        for (var i = 0; i < currentModels.length; i ++) {
+            var model = currentModels[i].toLowerCase() + ' ';
+
+            if (0 === model.indexOf(math)) {
+                return true;
+            }
+        }
+
+        return false;
+    };
 })();
